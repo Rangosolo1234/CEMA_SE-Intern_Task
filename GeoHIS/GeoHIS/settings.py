244d95e9ce18609c560ebf1be75dfea4e346c2d6
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-04-27 00:08:11 +0300

    Setting path for gdal

--- GeoHIS/GeoHIS/settings.py
+++ GeoHIS/GeoHIS/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH', r'C:\Users\alber\Envs\codedreamer\Lib\site-packages\osgeo\gdal305.dll') 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
