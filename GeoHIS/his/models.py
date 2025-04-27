d6483c051c522fc5cbeb071288d476771cee25d9
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-04-27 06:28:58 +0300

    Added a model for registering a health program

--- GeoHIS/his/models.py
+++ GeoHIS/his/models.py
@@ -11,3 +11,10 @@ class Hospital(models.Model):
     location = models.PointField()
     address = models.TextField()
 
+ ==================model to register a ealth program=================
+class HealthProgram(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    hospitals = models.ManyToManyField(Hospital)
+
