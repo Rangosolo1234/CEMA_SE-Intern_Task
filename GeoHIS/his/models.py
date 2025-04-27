958333d51f9f832116b5bfcf65a387461f297166
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-04-27 06:25:50 +0300

    Model to allow hospitals to register into the app

--- GeoHIS/his/models.py
+++ GeoHIS/his/models.py
@@ -1,3 +1,13 @@
+from django.contrib.gis.db import models
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+#===================model for hospital registration ============================
+class Hospital(models.Model):
+    name = models.CharField(max_length=255) 
+    location = models.PointField()
+    address = models.TextField()
+
