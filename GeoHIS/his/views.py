e2a77ee5b8c9b19e7f512a83dd0a1b9494ea0570
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-04-27 08:25:44 +0300

    Created views for serialized api models

--- GeoHIS/his/views.py
+++ GeoHIS/his/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework_gis.viewsets import GeoModelViewSet
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+#===============view for hospitals =============
+class HospitalViewSet(GeoModelViewSet):
+    queryset = Hospital.objects.all()
+    serializer_class = HospitalSerializer
+
+#==============view for health programs =============
+class HealthProgramViewSet(viewsets.ModelViewSet):
+    queryset = HealthProgram.objects.all()
+    serializer_class = HealthProgramSerializer
+
+#===========view for doctor profiles ==================
+class DoctorProfileViewSet(viewsets.ModelViewSet):
+    queryset = DoctorProfile.objects.all()
+    serializer_class = DoctorProfileSerializer
+
+#=========== View for clients profies==============
+class ClientViewSet(GeoModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+#===========view for appointment boking============
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+#=================view for blog posts ==============
+class BlogPostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+#===================view for incidences==============
+class IncidenceViewSet(GeoModelViewSet):
+    queryset = Incidence.objects.all()
+    serializer_class = IncidenceSerializer
