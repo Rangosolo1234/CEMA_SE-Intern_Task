0d2b425a3fbdf96157bd3888745e3b9317cb7101
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-04-27 15:56:28 +0300

    Included app's urls in the projects urls

--- GeoHIS/GeoHIS/urls.py
+++ GeoHIS/GeoHIS/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include(his.urls))
+    path('', include('his.urls')),
     path('admin/', admin.site.urls),
 ]
