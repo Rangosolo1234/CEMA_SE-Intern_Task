4f017c0288d8bd02ab8d38a0706b20f2070705f4
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-04-27 06:39:31 +0300

    Clients registration model

--- GeoHIS/his/models.py
+++ GeoHIS/his/models.py
@@ -26,3 +26,12 @@ class DoctorProfile(models.Model):
     short_description = models.TextField()
     programs_responsible = models.ManyToManyField(HealthProgram, related_name='doctors')
 
+#================Client=====================
+class Client(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    location = models.PointField()
+    health_history = models.TextField()
+    age = models.PositiveIntegerField()
+    programs_joined = models.ManyToManyField(HealthProgram)
+    registered_by = models.ForeignKey(DoctorProfile, null=True, blank=True, on_delete=models.SET_NULL)
