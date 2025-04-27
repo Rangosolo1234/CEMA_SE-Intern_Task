e953236586e45d4828fb7c10f37985a77e730d41
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-04-27 15:55:25 +0300

    Updated setting for installed apps, added leaflet, rest_framework

--- GeoHIS/GeoHIS/settings.py
+++ GeoHIS/GeoHIS/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'rest_framework',
+    'rest_framework_gis',
+    'leaflet',
     'his',
 ]
 
